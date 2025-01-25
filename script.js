deb38cc3d0236d6c306d53e8b8df6b879c3ec239
SVcreate <vitazoslav.vajda.ml@gmail.com>
2025-01-25 13:53:46 +0100

    yea

---
+++ script.js
@@ -0,0 +1,24 @@
+function getLang() {
+  if (navigator.languages !== undefined) 
+    return navigator.languages[0]; 
+  return navigator.language;
+}
+
+window.addEventListener("load", () => {
+  let previousPosition = 0.0;
+  const nav = document.querySelector("nav");
+
+  addEventListener("scroll", () => {
+    const currentPositon = window.scrollY;
+
+    if (currentPositon > 200.0 && currentPositon > previousPosition) {
+      nav.classList.add("scrolling");
+    } else {
+      nav.classList.remove("scrolling");
+    };
+
+    previousPosition = currentPositon;
+  });
+
+  console.log(getLang())
+});
